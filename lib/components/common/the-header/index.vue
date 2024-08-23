bfd0982c2be6aee8813238a2403f11f5cae7422b
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-08-23 09:44:48 +0700

    1

---
+++ lib/components/common/the-header/index.vue
@@ -0,0 +1,140 @@
+<template>
+	<header
+		id="header"
+		class="fixed flex items-center w-full h-[64px] md:h-[84px] xl:h-[90px] max-w-full px-6 md:px-6 lg:px-10 xl:px-auto z-[100] inset-0 bg-white rounded-es-[20px] rounded-ee-[20px]"
+	>
+		<div class="w-full max-w-[1730px] mx-auto">
+			<div class="w-full h-full my-auto lg:py-5 flex justify-between items-center">
+				<!--@@@ LOGO -->
+				<nuxt-link
+					to="/"
+					class="inline-flex relative w-[181px] h-[41px] md:w-[167px] md:h-[37px] xl:w-[207px] xl:h-[47px]"
+				>
+					<nuxt-img
+						src="/logo/logo-full-v2.png"
+						alt="UPS"
+						width="207"
+						height="47"
+						loading="lazy"
+						class="absolute w-full h-full object-contain"
+					/>
+				</nuxt-link>
+
+				<div class="flex gap-10 items-center">
+					<!--@@@ SEARCH  -->
+					<div class="hidden xl:inline-flex w-6 h-6 relative cursor-pointer">
+						<nuxt-img
+							src="/icons/search-normal.svg"
+							alt="UPS"
+							loading="lazy"
+							height="24"
+							class="absolute w-full h-full object-contain"
+						/>
+					</div>
+
+					<!-- NAV -->
+					<navigation />
+
+					<div class="flex gap-5">
+						<div class="hidden xl:inline-flex">
+							<nuxt-link
+								to="/giao-dich-ngay"
+								class="w-auto h-[50px] px-7 gap-2 inline-flex items-center rounded-[40px] text-base font-bold bg-[rgba(0,0,0,0.06)] duration-300"
+							>
+								<span> Giao dịch ngay </span>
+								<!-- <div class="inline-flex gap-1 w-4 h-4 overflow-hidden relative">
+								<i
+									class="w-full h-full inline-flex absolute transition-all duration-300 translate-x-[-120%] group-hover:translate-x-0"
+								>
+									<nuxt-img
+										src="/icons/arrow-icon.svg"
+										loading="lazy"
+										width="16"
+										height="16"
+										alt="UPS"
+									/>
+								</i>
+
+								<i
+									class="w-full h-full inline-flex absolute transition-all duration-300 translate-x-0 group-hover:translate-x-[120%]"
+								>
+									<nuxt-img
+										src="/icons/arrow-icon.svg"
+										loading="lazy"
+										width="16"
+										height="16"
+										alt="UPS"
+									/>
+								</i>
+							</div> -->
+							</nuxt-link>
+						</div>
+
+						<div class="hidden xl:inline-flex">
+							<nuxt-link
+								to="/"
+								class="btn active group"
+							>
+								<span> Mở tài khoản ngay </span>
+								<div class="inline-flex gap-1 w-4 h-4 overflow-hidden relative">
+									<i class="w-full h-full group-hover:inline-flex hidden absolute">
+										<nuxt-img
+											src="/icons/arrow-dark-icon.svg"
+											loading="lazy"
+											width="16"
+											height="16"
+											alt="UPS"
+										/>
+									</i>
+
+									<i class="w-full h-full group-hover:hidden inline-flex absolute">
+										<nuxt-img
+											src="/icons/arrow-icon.svg"
+											loading="lazy"
+											width="16"
+											height="16"
+											alt="UPS"
+										/>
+									</i>
+								</div>
+							</nuxt-link>
+						</div>
+
+						<!-- MENU MOBILE -->
+						<div class="flex xl:hidden">
+							<div class="w-6 h-6 relative inline-flex items-center justify-center cursor-pointer">
+								<nuxt-img
+									format="webp"
+									loading="lazy"
+									height="24"
+									alt="UPS"
+									src="/icons/menu-icon.svg"
+									class="absolute w-full h-full object-contain"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NuxtButton from "~/components/ui/button/Button.vue";
+import Navigation from "./nav/index.vue";
+
+export default defineComponent({
+	name: "the-header",
+	components: {
+		NuxtButton,
+		Navigation,
+	},
+	setup(props, ctx) {
+		return {};
+	},
+});
+</script>
+
+<style></style>
---
+++ lib/components/home/ups-news/index.vue
@@ -0,0 +1,210 @@
+<template>
+	<section class="section_news animate-ups container">
+		<div
+			class="flex flex-col xl:flex-row gap-6 md:gap-10 xl:gap-14 items-center pt-0 pb-10 md:py-[80px] lg:py-[100px] xl:py-[130px]"
+		>
+			<div class="w-full xl:w-[340px] flex flex-col gap-5 xl:gap-10">
+				<div class="animate-ups__item inline-flex relative w-[90px] h-[40px] xl:w-[140px] xl:h-[60px]">
+					<nuxt-img
+						format="webp"
+						loading="lazy"
+						width="140"
+						height="60"
+						alt="UPS"
+						src="/logo/logo-cap-dark.png"
+						class="absolute w-full h-full object-contain"
+					/>
+				</div>
+
+				<div>
+					<h3
+						class="animate-ups__item bg-[linear-gradient(269deg,#00C058_41.75%,#1FAB5F_110.86%)] bg-clip-text text-transparent font-BG text-[40px] md:text-[54px] xl:text-[80px] font-bold leading-[1.2]"
+					>
+						<span class="text-[#15171E]">Tin</span>
+						nổi bật
+					</h3>
+
+					<p class="animate-ups__item text-base text-[#000] font-normal">
+						Cập nhật các sản phẩm, dịch vụ mới nhất cũng như các hoạt động, sự kiện của UPS
+					</p>
+				</div>
+				<div class="flex gap-3">
+					<button
+						:class="
+							cn(`animate-ups__item h-10 px-6 bg-[#15171E] rounded-full group transition-all relative`, {
+								'cursor-auto opacity-75': !vSwiperIndex,
+							})
+						"
+						@click="prevVSwiperSlide"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="12"
+							viewBox="0 0 16 12"
+							fill="none"
+						>
+							<path
+								d="M0.46967 5.46967C0.176777 5.76256 0.176777 6.23744 0.46967 6.53033L5.24264 11.3033C5.53553 11.5962 6.01041 11.5962 6.3033 11.3033C6.59619 11.0104 6.59619 10.5355 6.3033 10.2426L2.06066 6L6.3033 1.75736C6.59619 1.46447 6.59619 0.989593 6.3033 0.696699C6.01041 0.403806 5.53553 0.403806 5.24264 0.696699L0.46967 5.46967ZM15.1421 6.75C15.5563 6.75 15.8921 6.41421 15.8921 6C15.8921 5.58579 15.5563 5.25 15.1421 5.25V6.75ZM1 6.75H15.1421V5.25H1V6.75Z"
+								fill="#02E56A"
+								:class="
+									cn('transition-all duration-300 group-hover:fill-[#15171E]', {
+										'group-hover:fill-[#02E56A]': !vSwiperIndex,
+									})
+								"
+							/>
+						</svg>
+					</button>
+
+					<button
+						:class="
+							cn(
+								'animate-ups__item h-10 px-6 bg-[#15171E] hover:bg-[#02E56A] rounded-full group transition-all duration-300',
+								{
+									'cursor-auto opacity-75 hover:bg-[#15171E]': vSwiperIndex === 2,
+								}
+							)
+						"
+						@click="nextVSwiperSlide"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="17"
+							height="12"
+							viewBox="0 0 17 12"
+							fill="none"
+						>
+							<path
+								d="M1.14215 5.25C0.727937 5.25 0.392151 5.58579 0.392151 6C0.392151 6.41421 0.727937 6.75 1.14215 6.75V5.25ZM15.8146 6.53033C16.1075 6.23744 16.1075 5.76256 15.8146 5.46967L11.0416 0.696699C10.7488 0.403806 10.2739 0.403806 9.98099 0.696699C9.68809 0.989593 9.68809 1.46447 9.98099 1.75736L14.2236 6L9.98099 10.2426C9.68809 10.5355 9.68809 11.0104 9.98099 11.3033C10.2739 11.5962 10.7488 11.5962 11.0416 11.3033L15.8146 6.53033ZM1.14215 6.75H15.2843V5.25H1.14215V6.75Z"
+								fill="#02E56A"
+								:class="
+									cn('transition-all duration-300 group-hover:fill-[#15171E]', {
+										'group-hover:fill-[#02E56A]': vSwiperIndex === 2,
+									})
+								"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+			<div class="w-full xl:w-[calc(100%-340px)]">
+				<swiper
+					:modules="[SwiperNavigation]"
+					:pagination="true"
+					:slides-per-view="1.2"
+					:loop="false"
+					:speed="800"
+					:space-between="12"
+					:initial-slide="vSwiperIndex"
+					@swiper="setVSwiperRef"
+					@slide-change="updateVSwiperIndex"
+					:breakpoints="{
+						480: {
+							slidesPerView: 1.2,
+							spaceBetween: 12,
+						},
+						768: {
+							slidesPerView: 2.2,
+							spaceBetween: 24,
+						},
+						992: {
+							slidesPerView: 2.2,
+							spaceBetween: 24,
+						},
+						1200: {
+							slidesPerView: 3.5,
+							spaceBetween: 40,
+						},
+					}"
+				>
+					<swiper-slide
+						v-for="(i, doc) in 5"
+						:key="i"
+					>
+						<div class="news_layout_item flex flex-col py-5 relative group">
+							<div
+								class="relative w-full h-[200px] md:h-[250px] xl:h-[288px] rounded-[14px] overflow-hidden"
+							>
+								<nuxt-img
+									format="webp"
+									loading="lazy"
+									alt="UPS"
+									width="350"
+									height="290"
+									src="/images/tin-tuc-01.png"
+									class="w-full h-full object-cover transition-all duration-300 group-hover:scale-110"
+								/>
+							</div>
+
+							<div class="mt-3 md:mt-5">
+								<div class="flex justify-between">
+									<p class="text-sm md:text-base xl:text-lg text-[#F05] font-medium">#hotnew</p>
+									<p class="text-sm md:text-base xl:text-lg text-[#9498A8] font-medium">
+										5 phút trước
+									</p>
+								</div>
+
+								<h4
+									class="text-sm md:text-base xl:text-xl text-black font-semibold line-clamp-1 mt-3 md:mt-6"
+								>
+									Nội dung tin tức
+								</h4>
+								<p class="text-sm md:text-base xl:text-lg text-[#3C4052] font-medium line-clamp-3 mt-2">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+									incididunt...
+								</p>
+							</div>
+
+							<nuxt-link
+								to="/"
+								class="absolute w-full h-full"
+							/>
+						</div>
+					</swiper-slide>
+				</swiper>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { SwiperAutoplay, SwiperNavigation } from "#imports";
+import { cn } from "~/lib/utils";
+
+export default defineComponent({
+	name: "ups-news",
+	components: {},
+	setup(props, ctx) {
+		let vSwiperRef: any = null;
+		const vSwiperIndex = ref<number>(0);
+
+		const setVSwiperRef = (swiper: any) => {
+			vSwiperRef = swiper;
+		};
+
+		const updateVSwiperIndex = () => {
+			console.log(vSwiperRef?.activeIndex);
+
+			vSwiperIndex.value = vSwiperRef?.activeIndex || 0;
+		};
+
+		const prevVSwiperSlide = () => vSwiperRef?.slidePrev();
+		const nextVSwiperSlide = () => vSwiperRef?.slideNext();
+
+		return {
+			cn,
+			vSwiperRef,
+			vSwiperIndex,
+			SwiperAutoplay,
+			SwiperNavigation,
+			setVSwiperRef,
+			prevVSwiperSlide,
+			nextVSwiperSlide,
+			updateVSwiperIndex,
+		};
+	},
+});
+</script>
+
+<style scoped></style>
