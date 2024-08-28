ffac8c7f8552704171dfc9404165d135bdbb86be
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-08-28 16:37:14 +0700

    1

--- components/home/ups-partner/index.vue
+++ components/home/ups-partner/index.vue
@@ -1,25 +1,19 @@
 <template>
 	<section class="pb-[60px] lg:pb-[100px] xl:pb-[200px]">
+		<div class="flex justify-center items-center gap-4 pb-[50px]">
+			<h3
+				class="animate-ups__item text-[#01C159] font-BG text-[20px] md:text-[32px] xl:text-[60px] text-center lg:text-left font-semibold leading-[1 text-[52px] font-semibold">
+				Khách hàng của</h3>
+			<div class="inline-flex relative w-[80px] h-[40px] md:w-[145px] md:h-[66px] xl:w-[132px] xl:h-[60px] mt-2">
+				<nuxt-img format="webp" loading="lazy" alt="UPS" width="132" height="60" src="/logo/logo-cap-dark.png"
+					class="animate-ups__item absolute w-full h-full object-cover" />
+			</div>
+		</div>
 		<client-only>
-			<vue3-marquee
-				:duration="40"
-				:gradient="true"
-				:gradient-color="[255, 255, 255]"
-				gradient-length="20%"
-			>
-				<div
-					v-for="doc in 8"
-					:key="doc"
-					class="ml-[300px] group"
-				>
-					<nuxt-img
-						format="webp"
-						loading="lazy"
-						alt="UPS"
-						height="60"
-						:src="`/partner/partner-0${doc}.png`"
-						class="cursor-pointer object-contain transition-all duration-200"
-					/>
+			<vue3-marquee :duration="50" :gradient="true" :gradient-color="[255, 255, 255]" gradient-length="20%">
+				<div v-for="doc in 8" :key="doc" class="mx-[80px] group py-2">
+					<nuxt-img format="webp" loading="lazy" alt="UPS" height="60" :src="`/partner/partner-0${doc}.png`"
+						class="cursor-pointer object-contain transition-all duration-200 grayscale group-hover:grayscale-0 group-hover:scale-110" />
 				</div>
 			</vue3-marquee>
 		</client-only>
@@ -28,8 +22,6 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
-// grayscale group-hover:grayscale-0 group-hover:scale-110
 
 export default defineComponent({
 	name: "ups-partner",
