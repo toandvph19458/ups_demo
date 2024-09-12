3f445e552fa00ac80ac76167fbb05b86f652c4d2
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-09-12 11:13:42 +0700

    1

--- components/common/cursor-dot/index.vue
+++ components/common/cursor-dot/index.vue
@@ -3,7 +3,7 @@
     class="cursor_dot fixed top-0 left-0 z-[9999] pointer-events-none bg-[#02E56A] rounded-full hidden xl:block lg:block"
   ></div>
   <div
-    class="cursor_dot_2 fixed top-0 left-0 z-[9999] pointer-events-none bg-[#15171E] rounded-full hidden xl:block lg:block"
+    class="cursor_dot_2 fixed top-0 left-0 z-[9999] pointer-events-none bg-[#15171E] rounded-full hidden xl:hidden lg:block"
   >
     <div
       to="/"
