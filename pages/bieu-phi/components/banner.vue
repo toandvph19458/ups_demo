34bdbe3d1299efc29e126da58faef1dae2a9cde9
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-09-20 16:36:31 +0700

    1

--- pages/bieu-phi/components/banner.vue
+++ pages/bieu-phi/components/banner.vue
@@ -20,7 +20,7 @@
         </div>
 
         <div
-          class="grid md:grid-cols-4 grid-cols-2 items-center gap-3 md:gap-2 lg:gap-3 2xl:gap-8 xl:gap-6 3xl:layout 3xl:gap-10 justify-center">
+          class="grid md:grid-cols-3 grid-cols-2 items-center gap-3 md:gap-2 lg:gap-3 2xl:gap-8 xl:gap-6 3xl:layout 3xl:gap-10 justify-center">
           <div
             class="relative overflow-hidden px-2 py-4 md:py-6 w-full flex 3xl:h-[230px] lg:h-[152px] 2xl:px-[40px] 2xl:py-[24px] 3xl:px-[24px] 3xl:py-[40px] lg:p-[32px_8px] flex-col justify-center items-center gap-3 flex-[1_0_0] lg:rounded-[24px] rounded-[12px] 2xl:rounded-xl group animate-ups__item bg-[rgba(255,255,255,0.10)] 3xl:gap-5">
             <!-- Background Gradient -->
@@ -45,47 +45,39 @@
             </p>
           </div>
 
-          <div
+          <!-- <div
             class="relative overflow-hidden px-2 py-4 md:py-6 w-full flex 3xl:h-[230px] lg:h-[152px] 2xl:px-[40px] 2xl:py-[24px] 3xl:px-[24px] 3xl:py-[40px] lg:p-[32px_8px] flex-col justify-center items-center gap-3 flex-[1_0_0] lg:rounded-[24px] rounded-[12px] 2xl:rounded-xl group animate-ups__item bg-[rgba(255,255,255,0.10)] 3xl:gap-5">
-            <!-- Background Gradient -->
             <div
               class="absolute inset-0 bg-gradient-to-b from-[rgba(68,49,139,0.09)] to-[rgba(68,49,139,0.90)] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
             </div>
-            <!-- Background Image -->
             <img src="/images/bieuphi.jpg"
               class="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               alt="Background Image" />
 
-            <!-- Icon -->
             <div
               class="relative w-[12px] h-[12px] md:w-[20px] md:h-[20px] 2xl:w-[24px] 2xl:h-[24px] flex-shrink-0 z-20 group-hover:filter group-hover:brightness-0 group-hover:invert transition-colors duration-300">
               <nuxt-img src="/logo/logo-row.png" alt="" />
             </div>
 
-            <!-- Text -->
             <p
               class="text-white text-center text-[14px] md:text-[12px] font-BG font-semibold 3xl:text-[24px] 2xl:text-[20px] xl:text-[20px] lg:text-[18px] leading-normal z-30 transition-colors duration-300">
               Biểu phí giao dịch chứng khoán phái sinh
             </p>
-          </div>
+          </div> -->
 
           <div
             class="relative overflow-hidden px-2 py-4 md:py-6 w-full flex 3xl:h-[230px] lg:h-[152px] 2xl:px-[40px] 2xl:py-[24px] 3xl:px-[24px] 3xl:py-[40px] lg:p-[32px_8px] flex-col justify-center items-center gap-3 flex-[1_0_0] lg:rounded-[24px] rounded-[12px] 2xl:rounded-xl group animate-ups__item bg-[rgba(255,255,255,0.10)] 3xl:gap-5">
-            <!-- Background Gradient -->
             <div
               class="absolute inset-0 bg-gradient-to-b from-[rgba(68,49,139,0.09)] to-[rgba(68,49,139,0.90)] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
             </div>
-            <!-- Background Image -->
             <img src="/images/bieuphi.jpg"
               class="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300"
               alt="Background Image" />
 
-            <!-- Icon -->
             <div
               class="relative w-[12px] h-[12px] md:w-[20px] md:h-[20px] 2xl:w-[24px] 2xl:h-[24px] flex-shrink-0 z-20 group-hover:filter group-hover:brightness-0 group-hover:invert transition-colors duration-300">
               <nuxt-img src="/logo/logo-row.png" alt="" />
             </div>
-            <!-- Text -->
             <p
               class="text-white text-center text-[14px] md:text-[12px] font-BG font-semibold 3xl:text-[24px] 2xl:text-[20px] xl:text-[20px] lg:text-[18px] leading-normal z-30 transition-colors duration-300">
               Biểu phí dịch vụ tài chính
